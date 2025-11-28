a49a21acf8f4aaf3ffcdd97261d216b5e273b420
Lunixose <quocjq@gmail.com>
2025-11-28 21:53:10 +0700

    Update flake.nix and lib.nix

--- flake.nix
+++ flake.nix
@@ -9,13 +9,35 @@
         "aarch64-linux"
       ];
 
-      imports = [
-        inputs.flake-parts.flakeModules.easyOverlay
-        ./overlays
-        ./parts/hosts.nix
-        ./parts/homes.nix
-        ./parts/shells.nix
-      ];
+      perSystem =
+        { system, ... }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+
+                unstable = import inputs.nixpkgs-unstable {
+                  system = final.system;
+                  config.allowUnfree = true;
+                };
+                lib = inputs.nixpkgs.lib.extend (
+                  self: super: { custom = import ./lib { inherit (inputs.nixpkgs) lib; }; }
+                );
+              })
+
+            ];
+            config = { };
+          };
+
+          imports = [
+            inputs.flake-parts.flakeModules.easyOverlay
+            # ./overlays
+            ./parts/hosts.nix
+            ./parts/homes.nix
+            ./parts/shells.nix
+          ];
+        };
     };
 
   inputs = {
--- overlays/lib.nix
+++ overlays/lib.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
-final: prev: {
+{
   lib = inputs.nixpkgs.lib.extend (
-    self: super: { custom = import ../lib { inherit (inputs.nixpkgs) lib; }; }
+    self: super: { custom = import ./lib { inherit (inputs.nixpkgs) lib; }; }
   );
 }
