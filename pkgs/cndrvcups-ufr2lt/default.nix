9d81e6bacbed58a8760d926dbd04f1980efee26a
Lunixose <quocjq@gmail.com>
2025-12-05 20:02:24 +0700

    Create driver package for my printer Update default.nix and result

--- pkgs/cndrvcups-ufr2lt/default.nix
+++ pkgs/cndrvcups-ufr2lt/default.nix
@@ -10,17 +10,27 @@
   libxml2,
   libjpeg,
   libgcrypt,
+  gnome2,
   autoPatchelfHook,
   makeWrapper,
 }:
+let
 
-stdenv.mkDerivation rec {
+  libxml2_13 = libxml2.overrideAttrs rec {
+    version = "2.13.8";
+    src = fetchurl {
+      url = "mirror://gnome/sources/libxml2/${lib.versions.majorMinor version}/libxml2-${version}.tar.xz";
+      hash = "sha256-J3KUyzMRmrcbK8gfL0Rem8lDW4k60VuyzSsOhZoO6Eo=";
+    };
+  };
+in
+stdenv.mkDerivation {
   pname = "cndrvcups-ufr2lt";
   version = "5.00.18";
 
   src = fetchurl {
     url = "http://gdlp01.c-wss.com/gds/0/0100005950/10/linux-UFRIILT-drv-v500-uken-18.tar.gz";
-    sha256 = "0ngjj0iwhb9q78vmcdqmp1q3jfddyv9nzzd0jik0khbb05083226"; # Run: nix-prefetch-url <url>
+    sha256 = "0ngjj0iwhb9q78vmcdqmp1q3jfddyv9nzzd0jik0khbb05083226";
   };
 
   nativeBuildInputs = [
@@ -34,9 +44,17 @@ stdenv.mkDerivation rec {
     gtk2
     glib
     gcc.cc.lib
-    libxml2
+    libxml2.out
+    libxml2_13
     libjpeg
     libgcrypt
+    gnome2.libglade
+  ];
+
+  # Explicitly tell autoPatchelfHook about runtime dependencies
+  runtimeDependencies = [
+    libxml2.out
+    gnome2.libglade
   ];
 
   unpackPhase = ''
