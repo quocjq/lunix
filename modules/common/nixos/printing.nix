cc6ed54042645d4c2f8693def032f8c932ee50bb
Lunixose <quocjq@gmail.com>
2025-12-05 16:59:42 +0700

    Update printing.nix

--- modules/common/nixos/printing.nix
+++ modules/common/nixos/printing.nix
@@ -12,18 +12,18 @@
         cups-bjnp
         carps-cups
       ];
-      avahi = {
-        enable = true;
-        nssmdns4 = true;
-        openFirewall = true;
-      };
-      ipp-usb.enable = true;
     };
-
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    ipp-usb.enable = true;
   };
   networking.firewall.enable = false;
   environment.systemPackages = with pkgs; [
     system-config-printer
+    ipp-usb
   ];
 
 }
